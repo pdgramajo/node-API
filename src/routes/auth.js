db7fe402696f7d63cb7b3c94f1941204c8891dba
Pablo Gramajo <pgramajo@jbknowledge.com>
2018-11-30 16:40:27 -0300

    database conection mssql  gearhost

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,12 +1,35 @@
 import express from 'express';
-
+import sql from 'mssql';
 const router = express.Router();
 
-router.post('/',(req,res)=>{
-   const { credentials } = req.body;
-    console.log('****** credentials',credentials);
-    
-    res.status(400).json({ errors: { global: 'invalid credentials' } });
+router.post('/', (req, res) => {
+    const { credentials } = req.body;
+    console.log('****** credentials', credentials);
+
+    const config = {
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        server: process.env.SERVER,
+        database: process.env.database
+    };
+    const value = 1;
+
+    sql.connect(config).then(() => {
+        //return sql.query`select * from Authors where id = ${value}`
+        return sql.query`select top 300 * from [tblPosts]`
+    }).then(result => {
+        res.status(200).json({ data: result });
+    }).catch(err => {
+        // ... error checks
+        res.status(400).json({ errors: { global: err } })
+    })
+
+    sql.on('error', err => {
+        // ... error handler
+        res.status(400).json({ errors: { global2: err } })
+    })
+
+
 });
 
 
